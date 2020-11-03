9e86513c4bac194b5f4279b25423095b87045a46
cesar <cesar@PC-PORTATIL>
2020-11-03 16:24:51 +0100

    # WARNING: head commit changed in the meantime
    Ejercicio 1 de la unidad 2 resuelto

---
+++ src/unidad2/Division.java
@@ -0,0 +1,13 @@
+package unidad2;
+
+public class Division {
+
+	public static void main(String[] args) {
+		int num1=1234;
+		int num2=532;
+		double numero=num1/num2;
+		System.out.printf("1234/532=%15.2f", numero);
+
+	}
+
+}
